9dbd6c63eee6f2832a86e09bccb67a946f68ec51
Todd Price <toddwprice@gmail.com>
2016-01-18 19:36:08 -0600

    fixed missing diretory for run mode newFunc

--- lamb/tasks/newFunc.js
+++ lamb/tasks/newFunc.js
@@ -12,7 +12,7 @@ var main = function(func) {
 
   //copy files and replace template text
   var devRoot = path.join(process.cwd(), 'lamb/files');
-  var runRoot = path.join(process.cwd(), 'node_modules/lamb/files');
+  var runRoot = path.join(process.cwd(), 'node_modules/lambinator/lamb/files');
   var lambRoot = fs.existsSync(runRoot) ? runRoot : devRoot; 
   gulp.src([lambRoot + '/**', '!' + lambRoot + '/.env.sample', '!' + lambRoot + '/function-name.js'])
     .pipe(replace('{{function-name}}', func))
